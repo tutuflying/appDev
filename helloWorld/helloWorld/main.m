3ae8dd07f73242c38e15b9a838b4d6ed19c4612b
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-03-03 16:24:11 +0800

    test

---
+++ helloWorld/helloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  helloWorld
+//
+//  Created by Eric Zhao on 14-3-3.
+//  Copyright (c) 2014 Eric Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+
+    return 0;
+}
