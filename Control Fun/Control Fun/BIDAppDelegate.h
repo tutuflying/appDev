fe0a84bbe846849aab3802f5e52cca3f74e492cd
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-04-16 17:41:19 +0800

    test

---
+++ Control Fun/Control Fun/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Control Fun
+//
+//  Created by Eric Zhao on 14-4-16.
+//  Copyright (c) 2014年 Eric Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Control Fun/Control Fun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Control Fun
+//
+//  Created by Eric Zhao on 14-4-16.
+//  Copyright (c) 2014年 Eric Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
