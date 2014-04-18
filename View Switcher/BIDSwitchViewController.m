3399b7962b4d949c408c9e5fab62ba47873b9d0d
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-04-18 17:26:28 +0800

    test

--- View Switcher/BIDSwitchViewController.m
+++ View Switcher/BIDSwitchViewController.m
@@ -44,17 +44,22 @@
 }
 
 - (IBAction)switchViews:(id)sender {
+    [UIView beginAnimations:@"View Flip" context:nil];
+    [UIView setAnimationDuration:1.25];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
     if (self.yellowViewController.view.superview == nil) {
         if (self.yellowViewController == nil) {
             self.yellowViewController = [[BIDYellowViewController alloc] initWithNibName:@"YellowView" bundle:nil];
         }
-        [self.yellowViewController.view removeFromSuperview];
+        [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
+        [self.blueViewController.view removeFromSuperview];
         [self.view insertSubview:self.yellowViewController.view atIndex:0];
     } else {
         if (self.blueViewController == nil) {
             self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
         }
-        [self.blueViewController.view removeFromSuperview];
+        [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromLeft forView:self.view cache:YES];
+        [self.yellowViewController.view removeFromSuperview];
         [self.view insertSubview:self.blueViewController.view atIndex:0];
     }
 }
